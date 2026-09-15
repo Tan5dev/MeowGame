7018bf927747400ccffe2497fec04840e27fcb40
Tan5dev <tanmaypathe4@gmail.com>
2026-09-15 19:54:29 +0530

    Added level scenes

--- Scenes/title_screen.gd
+++ Scenes/title_screen.gd
@@ -17,3 +17,7 @@ func _on_begin_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
